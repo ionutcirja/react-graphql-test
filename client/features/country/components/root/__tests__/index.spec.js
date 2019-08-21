ef72b1d94009dbcc0257653c3bba8f33b8ee2295
Ionut Cirja <ionut.cirja@gmail.com>
2019-08-21 18:28:44 +0100

    create country component

---
+++ client/features/country/components/root/__tests__/index.spec.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { MockedProvider } from '@apollo/react-testing';
+import { render, cleanup } from '@testing-library/react';
+import { GraphQLError } from 'graphql';
+import wait from 'waait';
+import Country, { GET_COUNTRY_QUERY } from '..';
+
+describe('Country component', () => {
+  afterEach(cleanup);
+  
+  it('should render a link to the countries route of location continent query param is not passed', () => {
+    const { container } = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <MemoryRouter>
+          <Country
+            match={{ params: { code: 'DE' } }}
+            location={{ search: '' }}
+          />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+    
+    expect(container.querySelector('a[href="/countries"]')).toBeDefined();
+  });
+  
+  it('should render a link to the continent route of location continent query param is passed', () => {
+    const { container } = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <MemoryRouter>
+          <Country
+            match={{ params: { code: 'DE' } }}
+            location={{ search: '?continent=EU' }}
+          />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+    
+    expect(container.querySelector('a[href="/continents/EU"]')).toBeDefined();
+  });
+  
+  it('should render a loading message on query start', () => {
+    const { getByText } = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <MemoryRouter>
+          <Country
+            match={{ params: { code: 'DE' } }}
+            location={{ search: '' }}
+          />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+    
+    expect(getByText('Loading country data...')).toBeDefined();
+  });
+  
+  it('should render an error message on query fail', async () => {
+    const mocks = [
+      {
+        request: {
+          query: GET_COUNTRY_QUERY,
+          variables: {
+            code: 'DE',
+          },
+        },
+        result: {
+          errors: [new GraphQLError('Loading error')],
+          data: {},
+        },
+      },
+    ];
+    const { getByText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter>
+          <Country
+            match={{ params: { code: 'DE' } }}
+            location={{ search: '' }}
+          />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+    await wait(0);
+    
+    expect(getByText(/Loading error/)).toBeDefined();
+  });
+  
+  it('should render country data on query success', async () => {
+    const country = {
+      name: 'Denmark',
+      code: 'DK',
+      native: 'Danmark',
+      phone: '45',
+      currency: 'DKK',
+      continent: {
+        name: 'Europe',
+      },
+      languages: [
+        {
+          name: 'Danish',
+          native: 'Dansk',
+        },
+      ],
+    };
+    
+    const mocks = [
+      {
+        request: {
+          query: GET_COUNTRY_QUERY,
+          variables: {
+            code: 'DK',
+          },
+        },
+        result: {
+          data: {
+            country,
+          },
+        },
+      },
+    ];
+    const { getByText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter>
+          <Country
+            match={{ params: { code: 'DK' } }}
+            location={{ search: '' }}
+          />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+    await wait(0);
+    
+    expect(getByText(/Denmark/)).toBeDefined();
+    expect(getByText(/Danmark/)).toBeDefined();
+    expect(getByText(/45/)).toBeDefined();
+    expect(getByText(/DKK/)).toBeDefined();
+    expect(getByText(/Danish/)).toBeDefined();
+    expect(getByText(/Dansk/)).toBeDefined();
+  });
+});
--- client/features/country/components/root/index.js
+++ client/features/country/components/root/index.js
@@ -1,10 +1,132 @@
 // @flow
 import React from 'react';
+import { Link } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import { Maybe } from 'monet';
+import queryString from 'query-string';
+import type { Country } from 'types';
 
-const Country = () => (
-  <div>
-    <span>country details</span>
-  </div>
+type Props = {
+  match: {
+    params: {
+      code?: string,
+    },
+  },
+  location: {
+    search: string,
+  },
+}
+
+type CountryData = {|
+  country: Country,
+|}
+
+type ErrorData = {|
+  message: string,
+|}
+
+type Response = CountryData | ErrorData
+
+type QueryVars = {
+  code?: string,
+}
+
+export const GET_COUNTRY_QUERY = gql`
+  query getCountry($code: String) {
+    country(code: $code) {
+      name,
+      native,
+      phone,
+      continent {
+        name
+      },
+      currency,
+      languages {
+        name,
+        native
+      }
+    }
+  }
+`;
+
+const renderLoading = () => (
+  <span>Loading country data...</span>
 );
 
-export default Country;
+const renderError = (message?: String) => (
+  Maybe.fromNull(message)
+    .map((value) => (<span>{value}</span>))
+    .orSome('')
+);
+
+const renderData = ({
+  name,
+  native,
+  continent,
+  phone,
+  currency,
+  languages,
+}: Country) => (
+  <>
+    <span>{name}</span>
+    {Maybe.fromNull(native)
+      .map((value) => (<span>{`Native name: ${value}`}</span>))
+      .orSome('')}
+    {Maybe.fromNull(continent)
+      .map((value) => (<span>{`Continent: ${value.name}`}</span>))
+      .orSome('')}
+    {Maybe.fromNull(phone)
+      .map((value) => (<span>{`Phone prefix: ${value}`}</span>))
+      .orSome('')}
+    {Maybe.fromNull(currency)
+      .map((value) => (<span>{`Currency: ${value}`}</span>))
+      .orSome('')}
+    {Maybe.fromNull(languages)
+      .map((value) => (
+        <>
+          <span>Languages</span>
+          <ul>
+            {value.map(({ name, native }) => ( // eslint-disable-line no-shadow
+              <li key={name}>
+                {`${name} (Native name: ${native})`}
+              </li>
+            ))}
+          </ul>
+        </>
+      ))
+      .orSome('')}
+  </>
+);
+
+const CountryInfo = ({ match, location }: Props) => {
+  const queries = queryString.parse(location.search);
+  const backLink = Maybe.fromNull(queries.continent)
+    .fold('/countries')(
+      (value) => `/continents/${value}`,
+    );
+  
+  const { loading, error, data } = useQuery<Response, QueryVars>(
+    GET_COUNTRY_QUERY,
+    { variables: { code: match.params.code } },
+  );
+  
+  return (
+    <div>
+      <Link to={backLink}>
+        Back
+      </Link>
+      {Maybe.fromFalsy(loading)
+        .map(() => renderLoading())
+        .orSome('')}
+      {Maybe.fromNull(error)
+        .map((value) => renderError(value.message))
+        .orSome('')}
+      {Maybe.fromNull(data)
+        .map((value) => renderData({ ...value.country }))
+        .orSome('')}
+    </div>
+  );
+};
+
+export default CountryInfo;
